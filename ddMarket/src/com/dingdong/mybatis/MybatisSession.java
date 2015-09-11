51b8cb44e0e413565ee3352c7171d040db5ef471
yinbro <yinbro@foxmail.com>
2015-09-11 16:26:55 +0800

    测试通过MyBatis 下一步进行细致的分包和接口涉及

---
+++ ddMarket/src/com/dingdong/mybatis/MybatisSession.java
@@ -0,0 +1,63 @@
+package com.dingdong.mybatis;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.dingdong.dao.UserMapper;
+import com.dingdong.pojo.User;
+
+public class MybatisSession {
+	
+	
+	public SqlSession getSession() throws IOException {
+		//mybatis的配置文件
+	    InputStream is =  this.getClass().getClassLoader().getSystemResourceAsStream("com/dingdong/mybatis/mybatis-config.xml");
+	    //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
+	    //构建sqlSession的工厂
+	    
+	    /*测试读取是xml是否正常
+	    InputStreamReader isr = new InputStreamReader(is);
+	    BufferedReader buffReader = new BufferedReader(isr);
+
+	    StringBuffer sb = new StringBuffer();
+	    while (buffReader.read() != -1) {
+			sb.append(buffReader.readLine());
+		}
+	    
+	    System.out.println("mybatis-cofig.xml:");
+	    System.out.println(sb.toString());
+	    */
+	    
+	    SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+	    
+	    SqlSession session = sessionFactory.openSession();
+		return session;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		MybatisSession ms = new MybatisSession();
+		SqlSession session = ms.getSession();
+		User user = new User();
+		user.setName("yinbro");
+		user.setEmail("yinbro@foxmail.com");
+		user.setPassword("1234");
+		user.setCredit(5);
+		
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		int status = userMapper.insert(user);
+		System.out.println(status);
+		
+		
+		session.commit();
+		session.close();
+	}
+	
+	
+	
+}
---
+++ ddMarket/src/com/dingdong/service/UserService.java
@@ -0,0 +1,46 @@
+package com.dingdong.service;
+
+import com.dingdong.dao.UserMapper;
+import com.dingdong.pojo.User;
+
+public class UserService implements UserMapper{
+
+	@Override
+	public int deleteByPrimaryKey(Integer userId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insert(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertSelective(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public User selectByPrimaryKey(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateByPrimaryKey(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+}
